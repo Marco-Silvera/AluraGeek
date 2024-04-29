f8024341e58049ee0284ca051090fb9b9373894b
Marco Silvera <2018019183@unfv.edu.pe>
2024-04-28 23:52:53 -0500

    Prueba de actualización de productos

--- js/crearProducto.js
+++ js/crearProducto.js
@@ -11,11 +11,25 @@ async function crearProducto(evento) {
     const precio = document.querySelector('[data-precio]').value;
     const imagen = document.querySelector('[data-imagen]').value;
 
-    await conexionAPI.enviarProducto(nombre, precio, imagen)
+    // await conexionAPI.enviarProducto(nombre, precio, imagen)
 
-    window.location.href = 'index.html';
+    // window.location.href = 'index.html';
 
-    console.log("info enviada");
+    // console.log("info enviada");
+
+
+
+    try {
+        // Enviamos el producto
+        await conexionAPI.enviarProducto(nombre, precio, imagen);
+
+        // Actualizamos la lista de productos
+        await listarProductos();
+
+        console.log("Producto agregado exitosamente.");
+    } catch (error) {
+        console.error("Error al agregar el producto:", error);
+    }
 }
 
 formulario.addEventListener('submit', evento => crearProducto(evento));
